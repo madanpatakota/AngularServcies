b945377426f0fc6ec665db8c0207e388a3aa0fd9
Madn <madan.patakota@gmail.com>
2024-06-21 09:54:33 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstCompComponent } from './first-comp/first-comp.component';
+import { SecondCompComponent } from './second-comp/second-comp.component';
+import { ThirdCompComponent } from './third-comp/third-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstCompComponent,
+    SecondCompComponent,
+    ThirdCompComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/first-comp/first-comp.component.ts
@@ -0,0 +1,80 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-comp',
+  templateUrl: './first-comp.component.html',
+  styleUrls: ['./first-comp.component.css']
+})
+export class FirstCompComponent {
+
+
+  //// Total 9 records of Employees and their details are 
+  //in the format of EmployeeID , FullName , City , PostalCode and Country
+  EmployeesList = [
+    {
+      EmployeeID: 1,
+      FullName: 'NancyDavolio',
+      City: 'Seattle',
+      PostalCode: '98122',
+      Country: 'USA',
+    },
+    {
+      EmployeeID: 2,
+      FullName: 'AndrewFuller',
+      City: 'Tacoma',
+      PostalCode: '98401',
+      Country: 'USA',
+    },
+    {
+      EmployeeID: 3,
+      FullName: 'JanetLeverling',
+      City: 'Kirkland',
+      PostalCode: '98033',
+      Country: 'USA',
+    },
+    {
+      EmployeeID: 4,
+      FullName: 'MargaretPeacock',
+      City: 'Redmond',
+      PostalCode: '98052',
+      Country: 'USA',
+    },
+    {
+      EmployeeID: 5,
+      FullName: 'StevenBuchanan',
+      City: 'London',
+      PostalCode: 'SW1 8JR',
+      Country: 'UK',
+    },
+    {
+      EmployeeID: 6,
+      FullName: 'MichaelSuyama',
+      City: 'London',
+      PostalCode: 'EC2 7JR',
+      Country: 'UK',
+    },
+    {
+      EmployeeID: 7,
+      FullName: 'RobertKing',
+      City: 'London',
+      PostalCode: 'RG1 9SP',
+      Country: 'UK',
+    },
+    {
+      EmployeeID: 8,
+      FullName: 'LauraCallahan',
+      City: 'Seattle',
+      PostalCode: '98105',
+      Country: 'USA',
+    },
+    {
+      EmployeeID: 9,
+      FullName: 'AnneDodsworth',
+      City: 'London',
+      PostalCode: 'WG2 7LT',
+      Country: 'UK',
+    },
+  ];
+
+
+}
